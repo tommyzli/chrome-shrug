0acfc4b868983cab6cb848490d4eba2a230f53bb
tommyzli <tommyli113@gmail.com>
2016-12-26 11:14:57 -0500

    lint

--- content_script.js
+++ content_script.js
@@ -1,10 +1,10 @@
 function onInput(event) {
   if (event.target.value.indexOf(":shrug:") !== -1) {
-    event.target.value = event.target.value.replace(":shrug:", "¯\\_(ツ)_/¯")
+    event.target.value = event.target.value.replace(":shrug:", "¯\\_(ツ)_/¯");
   }
 }
 
 var input_tags = document.querySelectorAll("input, textarea");
-input_tags.forEach(function (currentValue, index, arr) {
+input_tags.forEach(function (currentValue) {
   currentValue.addEventListener("input", onInput);
 });
